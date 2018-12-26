2fe7ef4e22a4aa26dfb657f1fc37c5904606e087
Chathura Dushmantha <dushmantha.1989@gmail.com>
2018-12-26 19:12:30 +0530

    Exception class added for specific error messages

---
+++ src/main/java/com/debugger/appium/spy/exceptions/NoDeviceConnectedException.java
@@ -0,0 +1,10 @@
+package com.debugger.appium.spy.exceptions;
+
+public class NoDeviceConnectedException extends Exception{
+
+	/**
+	 * Random generated Serial ID
+	 */
+	private static final long serialVersionUID = 8006061619764989016L;
+
+}
