8d0cdafdc4421d5c7f65d1576e4ba48a4e24a2dd
Chathura Dushmantha <dushmantha.1989@gmail.com>
2018-12-31 13:51:13 +0530

    Added os utils

---
+++ src/main/java/com/debugger/appium/spy/utils/OsUtils.java
@@ -0,0 +1,22 @@
+package com.debugger.appium.spy.utils;
+
+public final class OsUtils {
+	
+	private static String OS = null;
+
+	public static String getOsName() {
+		if (OS == null) {
+			OS = System.getProperty("os.name");
+		}
+		return OS;
+	}
+
+	public static boolean isWindows() {
+		return getOsName().startsWith("Windows");
+	}
+
+	public static boolean isUnix() {
+		return getOsName().contains("Mac");
+	}
+
+}
